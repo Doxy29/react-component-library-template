82147f427684b97e2aa1e1994e82e64269f6aea1
Gersi Shehaj <gersi.shehaj@sisal.al>
2023-11-13 12:25:09 +0100

    Update, bare bone

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,13 @@
+﻿import React from "react"
+
+type ButtonProps = {
+        label: string
+}
+
+const Button = (props: ButtonProps) =>{
+    return(
+        <button>{props.label}</button>
+    )
+}
+
+export default Button;
